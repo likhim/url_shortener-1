930cc59122717ecf5f6ca2f6e99fba757b5de671
user <likhimsiow@yahoo.com>
2017-01-09 18:14:35 +0800

    rspec for UserController

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,2 +1,24 @@
 class UrlsController < ApplicationController
+	# def index
+	# 	@url = Url.all
+	# end
+
+	def create
+		@url = Url.new(url_params)
+		@url.short_url = SecureRandom.base64[0..8]
+		if @url.save
+			redirect_to urls_path
+		else
+			@errors = @url.errors.full_messages
+			render :new
+		end
+	end
+
+	# def new
+	# end
+
+	private
+	def url_params
+		params.require(:url).permit(:long_url)
+	end
 end
